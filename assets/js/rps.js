97c39d71b915503e29b0bebe686bdd484164ab5a
jamie2210 <jamieletts2210@gmail.com>
2023-08-29 10:25:02 +0000

    Add javascript functions to html features

---
+++ assets/js/rps.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function() {
+   
+  });
+
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('.choice-btn')
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+}))
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length)
+
+  if (randomNumber === 0) {
+    computerChoice = 'rock'
+  }
+  if (randomNumber === 1) {
+    computerChoice = 'scissors'
+  }
+  if (randomNumber === 2) {
+    computerChoice = 'paper'
+  }
+  computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "It's a draw!"
+  }
+  if (computerChoice === 'rock' && userChoice === 'scissors') {
+    result = "You lose!"
+  }
+  if (computerChoice === 'rock' && userChoice === 'paper') {
+    result = "You win!"
+  }
+  if (computerChoice === 'scissors' && userChoice === 'rock') {
+    result = "You win!"
+  }
+  if (computerChoice === 'scissors' && userChoice === 'paper') {
+    result = "You lose!"
+  }
+  if (computerChoice === 'paper' && userChoice === 'rock') {
+    result = "You lose!"
+  }
+  if (computerChoice === 'paper' && userChoice === 'scissors') {
+    result = "You win!"
+  }
+  resultDisplay.innerHTML = result
+}
