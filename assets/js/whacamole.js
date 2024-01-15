dc9894e06eec9b9847edebb13c2b65c0303e06bd
jamie2210 <jamieletts2210@gmail.com>
2024-01-15 08:46:43 +0000

    Begin to build js code for game

---
+++ assets/js/whacamole.js
@@ -0,0 +1,18 @@
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+
+
+let result = 0
+
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('visibile')
+    });
+
+    let randomPosition = squares[Math.floor(Math.random() * 9)]
+    console.log(randomPosition)
+}
+
+randomSquare()
