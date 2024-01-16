10c1c27f5cd403de6a752f14957d6ae798ad3521
jamie2210 <jamieletts2210@gmail.com>
2024-01-16 09:37:00 +0000

    Add random selector for mole image

--- assets/js/whacamole.js
+++ assets/js/whacamole.js
@@ -6,13 +6,34 @@ const score = document.querySelector('#score')
 
 let result = 0
 
+// function randomSquare() {
+//     squares.forEach(square => {
+//         square.classList.remove('visibile')
+//     });
+
+//     let randomSquare = squares[Math.floor(Math.random() * 9)]
+//     randomSquare.classList.add('visible')
+//     randomSquare.classList.remove('mole')
+// }
+
 function randomSquare() {
     squares.forEach(square => {
-        square.classList.remove('visibile')
+        const imageElement = square.querySelector('img');
+
+        if(imageElement) {
+            imageElement.classList.remove('mole');
+        }
     });
 
-    let randomPosition = squares[Math.floor(Math.random() * 9)]
-    console.log(randomPosition)
+    let randomIndex = Math.floor(Math.random() * 9);
+    let randomSquare = squares[randomIndex];
+    
+    const selectedImage = randomSquare.querySelector('img');
+
+    if (selectedImage) {
+        selectedImage.classList.add('mole');
+        selectedImage.classList.remove('visible-no');
+    }
 }
 
 randomSquare()
