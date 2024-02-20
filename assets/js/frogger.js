4099e157335852692881baad779a0f3d39d5792a
jamie2210 <jamieletts2210@gmail.com>
2024-02-20 08:59:55 +0000

    Add function to movelogs left

--- assets/js/frogger.js
+++ assets/js/frogger.js
@@ -5,6 +5,7 @@ const pauseButton = document.querySelector('#pause-button');
 const startGameCount = document.querySelector('#start-game-count');
 const startText = document.querySelector('.start-text');
 const squares = document.querySelectorAll('.frogger-grid div');
+const logsLeft =document.querySelectorAll('.log-left');
 
 let startCountDown = 4;
 let currentIndex = 76
@@ -52,3 +53,36 @@ function startCount() {
         setTimeout(startCount, 1000);
     }
 }
+
+function autoMoveLogs() {
+    logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+}
+
+autoMoveLogs
+
+function moveLogLeft(logLeft) {
+    switch(true) {
+        case logLeft.classList.contains('l1') :
+            logLeft.classList.remove('l1')
+            logLeft.classList.add('l2')
+            break
+        case logLeft.classList.contains('l2') :
+            logLeft.classList.remove('l2')
+            logLeft.classList.add('l3')
+            break
+        case logLeft.classList.contains('l3') :
+            logLeft.classList.remove('l3')
+            logLeft.classList.add('l4')
+            break 
+        case logLeft.classList.contains('l4') :
+            logLeft.classList.remove('l4')
+            logLeft.classList.add('l5')
+            break
+        case logLeft.classList.contains('l5') :
+            logLeft.classList.remove('l5')
+            logLeft.classList.add('l1')
+            break    
+        }
+    }    
+
+setInterval(autoMoveLogs, 1000)
