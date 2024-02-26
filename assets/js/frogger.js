ea684fa6d0a4bb0efb5325fec35dddb0bb805cc4
jamie2210 <jamieletts2210@gmail.com>
2024-02-26 09:11:30 +0000

    add movement to logs and cars

--- assets/js/frogger.js
+++ assets/js/frogger.js
@@ -6,6 +6,9 @@ const startGameCount = document.querySelector('#start-game-count');
 const startText = document.querySelector('.start-text');
 const squares = document.querySelectorAll('.frogger-grid div');
 const logsLeft =document.querySelectorAll('.log-left');
+const logsRight = document.querySelectorAll('.log-right');
+const carsLeft = document.querySelectorAll('.car-left');
+const carsRight = document.querySelectorAll('.car-right');
 
 let startCountDown = 4;
 let currentIndex = 76
@@ -54,11 +57,15 @@ function startCount() {
     }
 }
 
-function autoMoveLogs() {
+function autoMoveElements() {
     logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+    logsRight.forEach(logRight => moveLogRight(logRight))
+    carsLeft.forEach(carLeft => moveCarLeft(carLeft))
+    carsRight.forEach(carRight => moveCarRight(carRight))
 }
 
-autoMoveLogs
+autoMoveElements
+setInterval(autoMoveElements, 1000)
 
 function moveLogLeft(logLeft) {
     switch(true) {
@@ -83,6 +90,65 @@ function moveLogLeft(logLeft) {
             logLeft.classList.add('l1')
             break    
         }
-    }    
+    }  
+    
+function moveLogRight(logRight) {
+    switch(true) {
+        case logRight.classList.contains('l1') :
+            logRight.classList.remove('l1')
+            logRight.classList.add('l5')
+            break
+        case logRight.classList.contains('l2') :
+            logRight.classList.remove('l2')
+            logRight.classList.add('l1')
+            break
+        case logRight.classList.contains('l3') :
+            logRight.classList.remove('l3')
+            logRight.classList.add('l2')
+            break 
+        case logRight.classList.contains('l4') :
+            logRight.classList.remove('l4')
+            logRight.classList.add('l3')
+            break
+        case logRight.classList.contains('l5') :
+            logRight.classList.remove('l5')
+            logRight.classList.add('l4')
+            break    
+        }
+    }
 
-setInterval(autoMoveLogs, 1000)
+function moveCarRight(carRight) {
+    switch(true) {
+        case carRight.classList.contains('c3') : 
+            carRight.classList.remove('c3')
+            carRight.classList.add('c2')
+            break
+        case carRight.classList.contains('c2') : 
+            carRight.classList.remove('c2')
+            carRight.classList.add('c1')
+            break
+        case carRight.classList.contains('c1') : 
+            carRight.classList.remove('c1')
+            carRight.classList.add('c3')
+            break
+    }
+}
+
+function moveCarLeft(carLeft) {
+    switch(true) {
+        case carLeft.classList.contains('c3') : 
+            carLeft.classList.remove('c3')
+            carLeft.classList.add('c1')
+            break
+        case carLeft.classList.contains('c1') : 
+            carLeft.classList.remove('c1')
+            carLeft.classList.add('c2')
+            break
+        case carLeft.classList.contains('c2') : 
+            carLeft.classList.remove('c2')
+            carLeft.classList.add('c3')
+            break
+    }
+}
+
+
